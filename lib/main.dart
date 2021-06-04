080f3a52ccea943fb9937031a45daa8a67eab75c
tjdans345 <tjdans345@naver.com>
2021-06-04 17:05:18 +0900

    http 통신 라이브러리 연결 완료

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,10 @@
+import 'package:dustcheckapp/models/AirResult.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+
+import 'dart:convert';
 
 void main() {
   runApp(MyApp());
@@ -24,82 +30,210 @@ class Main extends StatefulWidget {
 }
 
 class _MainState extends State<Main> {
+  AirResult? _result;
+
+  Future<AirResult> fetchData() async {
+    var uri = Uri.parse(
+        'http://api.airvisual.com/v2/nearest_city?lat=35.17944&lon=129.07556&key=c3942c05-d8a8-4855-aea9-8b4fc506a94c');
+    var response = await http.get(uri);
+    AirResult result = AirResult.fromJson(json.decode(response.body));
+    return result;
+  }
+
+  //생명주기 -> initState 초기에 한 번 실행함
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    fetchData().then((airResult) {
+      setState(() {
+        _result = airResult;
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                '현재 위치 미세먼지🌎',
-                style: TextStyle(fontSize: 30, color: Color(0xffff6229)),
-              ),
-              SizedBox(
-                height: 16.0,
-              ),
-              Card(
+      body: _result == null
+          ? Center(child: CircularProgressIndicator())
+          : Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Center(
                 child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
                   children: <Widget>[
-                    Container(
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Text('얼굴사진'),
-                          Text('80', style: TextStyle(fontSize: 40)),
-                          Text('보통', style: TextStyle(fontSize: 20)),
-                        ],
-                      ),
-                      padding: const EdgeInsets.all(8.0),
-                      color: Color(0xffa49cff),
+                    Text(
+                      '현재 위치 미세먼지',
+                      style: TextStyle(
+                          fontFamily: "ShongShong",
+                          fontSize: 30,
+                          color: Color(0xFF899FF3)),
                     ),
-                    Container(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          Row(
-                            children: [
-                              Text('이미지'),
-                              SizedBox(
-                                width: 16,
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    Card(
+                      child: Column(
+                        children: <Widget>[
+                          Container(
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  CircleAvatar(),
+                                  Text(
+                                      '${_result!.data!.current!.pollution!.aqius}',
+                                      style: TextStyle(fontSize: 30)),
+                                  Text(getString(_result),
+                                      style: TextStyle(fontSize: 20)),
+                                ],
                               ),
-                              Text(
-                                '11°',
-                                style: TextStyle(fontSize: 16),
-                              ),
-                            ],
-                          ),
-                          Text('습도 100%'),
-                          Text('풍속 5.7m/s'),
+                              padding: const EdgeInsets.all(8.0),
+                              color: getColor(_result)),
+                          Container(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceAround,
+                              children: [
+                                Row(
+                                  children: [
+                                    Image.network(
+                                      'https://airvisual.com/images/${_result!.data!.current!.weather!.ic}.png',
+                                      width: 32,
+                                      height: 32,
+                                    ),
+                                    SizedBox(
+                                      width: 16,
+                                    ),
+                                    Text(
+                                      '${_result!.data!.current!.weather!.tp}',
+                                      style: TextStyle(fontSize: 16),
+                                    ),
+                                  ],
+                                ),
+                                Text('${_result!.data!.current!.weather!.hu}%'),
+                                Text(
+                                    '${_result!.data!.current!.weather!.ws}m/s'),
+                              ],
+                            ),
+                          )
                         ],
                       ),
+                    ),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(30),
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          padding: const EdgeInsets.symmetric(
+                              vertical: 15.0, horizontal: 50),
+                          primary: Color(0xffb2aadb),
+                        ),
+                        onPressed: () {},
+                        child: Icon(
+                          Icons.refresh,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 16.0,
+                    ),
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Expanded(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  padding: const EdgeInsets.symmetric(
+                                      vertical: 15.0, horizontal: 50),
+                                  primary: Color(0xffcef2fe),
+                                ),
+                                onPressed: () {},
+                                child: Icon(
+                                  Icons.location_on,
+                                  color: Colors.white,
+                                )),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        Expanded(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 15.0, horizontal: 50),
+                                primary: Color(0xffa7d7ff),
+                              ),
+                              onPressed: () {},
+                              child: Text(
+                                '부산',
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        Expanded(
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.circular(10),
+                            child: ElevatedButton(
+                              style: ElevatedButton.styleFrom(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 15.0, horizontal: 50),
+                                primary: Color(0xffd3e4ff),
+                              ),
+                              onPressed: () {},
+                              child: Text(
+                                '양산',
+                                style: TextStyle(
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
                     )
                   ],
                 ),
               ),
-              SizedBox(
-                height: 16.0,
-              ),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(30),
-                child: RaisedButton(
-                  padding: const EdgeInsets.symmetric(
-                      vertical: 15.0, horizontal: 50),
-                  color: Color(0xff7567ff),
-                  onPressed: () {},
-                  child: Icon(
-                    Icons.refresh,
-                    color: Colors.white,
-                  ),
-                ),
-              )
-            ],
-          ),
-        ),
-      ),
+            ),
     );
+  }
+
+  Color? getColor(AirResult? result) {
+    if (result!.data!.current!.pollution!.aqius! <= 50) {
+      return Color(0xff7DF6B9);
+    } else if (result.data!.current!.pollution!.aqius! <= 100) {
+      return Color(0xffFCFC8E);
+    } else if (result.data!.current!.pollution!.aqius! <= 150) {
+      return Color(0xffFACDA2);
+    } else {
+      return Color(0xffFF9191);
+    }
+  }
+
+  String getString(AirResult? result) {
+    if (result!.data!.current!.pollution!.aqius! <= 50) {
+      return '좋음';
+    } else if (result.data!.current!.pollution!.aqius! <= 100) {
+      return '보통';
+    } else if (result.data!.current!.pollution!.aqius! <= 150) {
+      return '나쁨';
+    } else {
+      return '최악';
+    }
   }
 }
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,12 +5,25 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:dustcheckapp/models/AirResult.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:dustcheckapp/main.dart';
-import 'package:http/http.dart';
+import 'package:http/http.dart' as http;
+
+import 'dart:convert';
 
 void main() {
-  test('http 통신 테스트', () {});
+  test('http 통신 테스트', () async {
+    var uri = Uri.parse(
+        'http://api.airvisual.com/v2/nearest_city?key=c3942c05-d8a8-4855-aea9-8b4fc506a94c');
+    var response = await http.get(uri);
+
+    expect(response.statusCode, 200);
+
+    AirResult result = AirResult.fromJson(json.decode(response.body));
+    expect(result.status, 'success');
+    print(result.data!.city);
+  });
 }
